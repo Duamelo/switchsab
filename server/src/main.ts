1716fc27ef24d882f284c0cdd39360cb8d8feb96
Duamelo <david.dosseh@imsp-uac.org>
2022-08-05 03:09:56 +0200

    swagger documentation added

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('SWITCH SAB api description')
+    .setDescription(
+      'This API is the first version of switch sab software system',
+    )
+    .setVersion('1.0')
+    .addTag('game room station')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('documentation', app, document);
+
+  await app.listen(`${process.env.PORT}`);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
