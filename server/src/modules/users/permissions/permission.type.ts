7088433f6ed373a4b710cbbfee1de8eadb44f25f
Yann Kelly Olou <yolou@celeriteholding.com>
2022-08-06 15:55:50 +0100

    ajout des permissions de souscription

--- server/src/modules/users/permissions/permission.type.ts
+++ server/src/modules/users/permissions/permission.type.ts
@@ -2,14 +2,20 @@ import PostesPermission from "./postesPermission.enum";
 import CategoriesPermission from './categoriesPermission.enum';
 import GroupesPermission from "./groupesPermission.enum";
 import AutresPermission from "./autresPermission.enum";
+import SouscriptionsPermission from './souscriptionsPermission.enum';
 
 const Permission = {
   ...PostesPermission,
   ...CategoriesPermission,
   ...GroupesPermission,
+  ...SouscriptionsPermission,
   ...AutresPermission
 }
  
-type Permission = PostesPermission | CategoriesPermission | GroupesPermission | AutresPermission;
+type Permission = PostesPermission | 
+                  CategoriesPermission | 
+                  GroupesPermission | 
+                  SouscriptionsPermission | 
+                  AutresPermission;
  
 export default Permission;
